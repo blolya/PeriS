df16bf59e7459e1af7c3612c6dc04016da21b2f7
Dzyunka <mast-94@mail.ru>
2020-07-07 00:11:13 +0300

    Added port abstraction for gpio

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,14 @@ use cortex_m_rt::entry;
 use panic_reset as _;
 
 use peris::peripherals::gpio::gpioc::Gpioc;
-use peris::peripherals::gpio::Gpio;
+use peris::peripherals::gpio::Port;
 
 #[entry]
 fn main() -> ! {
-    {
-        let gpioc = Gpioc::new();
-        gpioc.set_port_mode(13, 0);
-        gpioc.set_port_speed(13, 2);
-        gpioc.set_port_high(13);
-    }
+    let gpioc = Gpioc::new();
+    let pc13 = Port::new(13, &gpioc);
+    pc13.set_mode(0);
+    pc13.set_speed(2);
+    pc13.set_high();
     loop {}
 }
--- src/peripherals/gpio.rs
+++ src/peripherals/gpio.rs
@@ -7,3 +7,25 @@ pub trait Gpio: Clocked {
     fn set_port_mode(&self, port: u32, mode: u32);
     fn set_port_speed(&self, port: u32, speed: u32);
 }
+
+pub struct Port<'a, T: Gpio> {
+    port_num: u32,
+    gpio: &'a T,
+}
+impl<'a, T: Gpio> Port<'a, T> {
+    pub fn new(port_num: u32, gpio: &'a T) -> Port<'a, T> {
+        Port { port_num, gpio }
+    }
+    pub fn set_high(&self) {
+        self.gpio.set_port_high(self.port_num);
+    }
+    pub fn set_low(&self) {
+        self.gpio.set_port_low(self.port_num);
+    }
+    pub fn set_mode(&self, mode: u32) {
+        self.gpio.set_port_mode(self.port_num, mode);
+    }
+    pub fn set_speed(&self, speed: u32) {
+        self.gpio.set_port_speed(self.port_num, speed);
+    }
+}
