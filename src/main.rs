f98cd8d49b1820d34a1504270da8294db2b79af5
blolya <mast-94@mail.ru>
2020-07-18 18:09:41 +0300

    Renamed Mode -> Type, Speed -> Mode

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@
 use cortex_m_rt::entry;
 use panic_reset as _;
 
-use peris::peripherals::gpio::{PortNum, PortMaxSpeed, Port, Mode, OutputConfig, gpioc::Gpioc};
+use peris::peripherals::gpio::{PortNum, PortMode, Port, PortType, OutputConfig, gpioc::Gpioc};
 
 
 #[entry]
 fn main() -> ! {
     let gpioc = Gpioc::new();
-    let mode = Mode::Output(OutputConfig::GeneralPurposePushPull( PortMaxSpeed::S2MHz ));
-    let p13 = Port::new(PortNum::P13, &gpioc, mode);
+    let mode = PortType::Output(OutputConfig::GeneralPurposePushPull( PortMode::S2MHz ));
+    let p13 = Port::new(PortNum::P13, mode, &gpioc);
     p13.set_high();
 
     loop {}
--- src/peripherals/gpio.rs
+++ src/peripherals/gpio.rs
@@ -16,49 +16,49 @@ pub struct Port<'a, T: Gpio> {
     gpio: &'a T,
 }
 impl<'a, T: Gpio> Port<'a, T> {
-    pub fn new(port_num: PortNum, gpio: &'a T, mode: Mode) -> Port<'a, T> {
+    pub fn new(port_num: PortNum, port_type: PortType, gpio: &'a T) -> Port<'a, T> {
         let port = Port { port_num, gpio };
-        port.set_mode(mode);
+        port.set_port_type(port_type);
         port
     }
     pub fn set_high(&self) {
-        let mode = self.get_mode();
+        let port_type = self.get_port_type();
         let port_num: u32 = (&self.port_num).into();
-        if let Mode::Input(_) = mode {
+        if let PortType::Input(_) = port_type {
             panic!("Port {} is in input mode. Consider to change port mode to output.", port_num);
         } else {
             self.gpio.set_port_output(port_num);
         }
     }
     pub fn set_low(&self) {
-        let mode = self.get_mode();
+        let port_type = self.get_port_type();
         let port_num: u32 = (&self.port_num).into();
-        if let Mode::Input(_) = mode {
+        if let PortType::Input(_) = port_type {
             panic!("Port {} is in input mode. Consider to change port mode to output.", port_num);
         } else {
             self.gpio.reset_port_output(port_num);
         }
     }
-    pub fn set_mode(&self, mode: Mode) {
-        let (bin_config, bin_speed) = mode.into();
+    pub fn set_port_type(&self, port_type: PortType) {
+        let (bin_config, bin_speed) = port_type.into();
         let port_num: u32 = (&self.port_num).into();
         self.gpio.set_port_config(port_num, bin_config);
         self.gpio.set_port_mode(port_num, bin_speed);
     }
-    pub fn get_mode(&self) -> Mode {
+    pub fn get_port_type(&self) -> PortType {
         let port_num: u32 = (&self.port_num).into();
         let bin_config = self.gpio.get_port_config(port_num);
         let bin_mode = self.gpio.get_port_mode(port_num);
-        let mode: Mode = (bin_config, bin_mode).into();
+        let mode: PortType = (bin_config, bin_mode).into();
         mode
     }
 }
 
 pub enum OutputConfig {
-    GeneralPurposePushPull(PortMaxSpeed),
-    GeneralPurposeOpenDrain(PortMaxSpeed),
-    AlternativeFunctionPushPull(PortMaxSpeed),
-    AlternativeFunctionOpenDrain(PortMaxSpeed),
+    GeneralPurposePushPull(PortMode),
+    GeneralPurposeOpenDrain(PortMode),
+    AlternativeFunctionPushPull(PortMode),
+    AlternativeFunctionOpenDrain(PortMode),
 }
 impl From<OutputConfig> for (u32, u32) {
     fn from(config: OutputConfig) -> (u32, u32) {
@@ -71,8 +71,8 @@ impl From<OutputConfig> for (u32, u32) {
     }
 }
 impl From<(u32, u32)> for OutputConfig {
-    fn from( (bin_config, bin_speed): (u32, u32) ) -> OutputConfig {
-        let speed: PortMaxSpeed = bin_speed.into();
+    fn from( (bin_config, bin_mode): (u32, u32) ) -> OutputConfig {
+        let speed: PortMode = bin_mode.into();
         match bin_config {
             0 => OutputConfig::GeneralPurposePushPull(speed),
             1 => OutputConfig::GeneralPurposeOpenDrain(speed),
@@ -108,30 +108,30 @@ impl From <(u32, u32)> for InputConfig {
         }
     }
 }
-pub enum Mode {
+pub enum PortType {
     Input(InputConfig),
     Output(OutputConfig),
 }
-impl From<Mode> for (u32, u32) {
-    fn from(mode: Mode) -> (u32, u32) {
-        match mode {
-            Mode::Input(config) => {
+impl From<PortType> for (u32, u32) {
+    fn from(port_type: PortType) -> (u32, u32) {
+        match port_type {
+            PortType::Input(config) => {
                 config.into()
             },
-            Mode::Output(config) => {
+            PortType::Output(config) => {
                 config.into()
             }
         }
     }
 }
-impl From<(u32, u32)> for Mode {
-    fn from( (bin_config, bin_speed): (u32, u32) ) -> Mode {
-        if bin_speed == 0 {
-            let config: InputConfig = (bin_config, bin_speed).into();
-            Mode::Input(config)
+impl From<(u32, u32)> for PortType {
+    fn from( (bin_config, bin_mode): (u32, u32) ) -> PortType {
+        if bin_mode == 0 {
+            let config: InputConfig = (bin_config, bin_mode).into();
+            PortType::Input(config)
         } else {
-            let config: OutputConfig = (bin_config, bin_speed).into();
-            Mode::Output(config)
+            let config: OutputConfig = (bin_config, bin_mode).into();
+            PortType::Output(config)
         }
     }
 }
@@ -163,19 +163,19 @@ impl From<u32> for PortNum {
         }
     }
 }
-pub enum PortMaxSpeed {
+pub enum PortMode {
     Reserved = 0,
     S2MHz = 2,
     S10MHz = 1,
     S50MHz = 3,
 }
-impl From<u32> for PortMaxSpeed {
-    fn from(bin_speed: u32) -> PortMaxSpeed {
+impl From<u32> for PortMode {
+    fn from(bin_speed: u32) -> PortMode {
         match bin_speed {
-            0 => PortMaxSpeed::Reserved,
-            1 => PortMaxSpeed::S10MHz,
-            2 => PortMaxSpeed::S2MHz,
-            3 => PortMaxSpeed::S50MHz,
+            0 => PortMode::Reserved,
+            1 => PortMode::S10MHz,
+            2 => PortMode::S2MHz,
+            3 => PortMode::S50MHz,
             _ => panic!(""),
         }
     }
