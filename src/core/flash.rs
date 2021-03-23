2d524d9914a979de8906b2c328d5878ae5a1a84a
blolya <mast-94@mail.ru>
2021-03-23 20:35:39 +0300

    Added additional registers to flash interface

--- src/core/flash.rs
+++ src/core/flash.rs
@@ -2,12 +2,26 @@ use super::register::Register;
 
 pub struct Flash {
     acr: Register,
+    keyr: Register,
+    optkeyr: Register,
+    sr: Register,
+    cr: Register,
+    ar: Register,
+    obr: Register,
+    wrpr: Register,
 }
 impl Flash {
     pub fn new() -> Flash {
         let address =  0x4002_2000;
         Flash {
             acr: Register::new(address),
+            keyr: Register::new(address + 0x04),
+            optkeyr: Register::new(address + 0x08),
+            sr: Register::new(address + 0x0c),
+            cr: Register::new(address + 0x10),
+            ar: Register::new(address + 0x14),
+            obr: Register::new(address + 0x1c),
+            wrpr: Register::new(address + 0x20),
         }
     }
     pub fn set_latency(&self, latency: u32) {
