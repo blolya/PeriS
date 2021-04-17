25bf2a36d024acc06694edf2e1180a778714611c
blolya <mast-94@mail.ru>
2021-04-17 14:06:46 +0300

    Clear main after merge

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,10 @@
 use cortex_m_rt::entry;
 use panic_reset as _;
 
-use peris::peripherals::{
-    fprom::Fprom,
-    communication::uart::usart1::Usart1,
-    clock,
-    ports::{
-        Port,
-        PortMode,
-        PortNum,
-        MaxSpeed,
-        OutputConfig
-    },
-};
-
-use peris::core::gpio::gpioc::Gpioc;
-use peris::core::flash::Flash;
+use peris::peripherals::clock;
 
 #[entry]
 fn main() -> ! {
     clock::init();
-
-    let mut fprom = Fprom::new(0x0000_f800);
-
-    fprom.erase();
-
     loop {}
 }
